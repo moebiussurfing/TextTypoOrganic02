e6d05d4a737d48d4e41e4e7e5c60fc3df204577d
moebiusSurfing <moebiussurfing@gmail.com>
2026-02-10 23:05:44 +0100

    Reduce debug overlay alpha values
    Lower DEBUG_ALPHA_MAX from 120 to 48 and DEBUG_ALPHA_MIN_OFFSET from 20 to 16 in organicTextConstants.h to reduce the opacity of debug overlays. This makes debug visuals subtler without changing other debug behavior.

--- src/organicTextConstants.h
+++ src/organicTextConstants.h
@@ -103,8 +103,8 @@ constexpr float SHAPE_SIZE_NOISE_SCALE = 0.5f;
 constexpr float SHAPE_SIZE_INDEX_SCALE = 0.01f;
 
 // Debug Constants
-constexpr int DEBUG_ALPHA_MAX = 120;
-constexpr int DEBUG_ALPHA_MIN_OFFSET = 20;
+constexpr int DEBUG_ALPHA_MAX = 48;
+constexpr int DEBUG_ALPHA_MIN_OFFSET = 16;
 constexpr float DEBUG_SPEED = .2f;
 
 // Outline Constants
